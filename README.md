b2ca1f8e38c8cd3822e52a51517af73fa6900a3f
rog-works <rog_works@yahoo.co.jp>
2018-01-14 14:26:58 +0900

    add: README.md

---
+++ README.md
@@ -0,0 +1,22 @@
+gdx2
+===
+
+# Requirements
+
+* Node.js
+* TypeScript
+* Docker
+* docker-compose
+
+# APIs
+
+* gdx2.files.list
+* gdx2.files.get 
+* gdx2.files.create
+* gdx2.files.update
+* gdx2.files.delete
+* gdx2.files.copy
+* gdx2.files.trash
+* gdx2.files.export
+* gdx2.files.watch
+* gdx2.files.generateIds
--- components/Gdx2.ts
+++ components/Gdx2.ts
@@ -1,3 +1,19 @@
-export class Gdx2 {
+namespace infos {
+	Info;
+	list(path: string = '/'): Info[];
+}
 
+namespace entries {
+	infos;
+	Entry;
+	create(path: string, content: any): Promise<void>;
+	get(path: string): Promise<Entry>;
+	update(path: string, content: any): Promise<void>;
+	delete(path: string): Promise<void>;
+}
+
+namespace gdx2 {
+	entries;
 };
+
+export gdx2;
